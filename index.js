8cb28429684ac5ba0c3ac75968b098ea35957d92
itshnik <rakhat.kzm@gmail.com>
2025-05-12 21:24:08 +0500

    Minor fixes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://shopsmart-client.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
